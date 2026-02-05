1e84178aef1283473f32efdf1ab90574291ed429
unknown <xavierrodgriues123@gmail.com>
2026-02-05 20:34:05 +0530

    fix: added deployment link backend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({ onLogin, onNavigate }) => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/v1/auth/login', {
+      const response = await fetch('https://redis-jobseeker.onrender.comapi/v1/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
